a77069258f46e28142e577679145e4f0f5d9ed22
John <wei@signerlabs.com>
2026-05-11 21:40:27 +0800

    Initial Commit

---
+++ TutorTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TutorTrack
+//
+//  Created by Wei Zhong on 11/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TutorTrack/TutorTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorTrackApp.swift
+//  TutorTrack
+//
+//  Created by Wei Zhong on 11/5/26.
+//
+
+import SwiftUI
+
+@main
+struct TutorTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
